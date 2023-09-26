d3ccf4c074e95207b31526864636e8cb99b3107d
manh2004 <146045144+Manhdoan2004@users.noreply.github.com>
2023-09-26 10:21:29 +0700

    Add files via upload

---
+++ TypeData/src/basic/dev/MainApp.java
@@ -0,0 +1,27 @@
+package basic.dev;
+
+public class MainApp {
+	
+	public static void main(String[] args) {
+		String name ="Doan Van Manh";
+		int idsv = 2360228;
+		int idnd = 123456789;
+		String nameclass = "laptrinh";
+		String nameschool = "CDCD";
+		float  dtb = 8f;
+		
+		System.out.println("Ho Va Ten: "+name);
+		System.out.println("Ma Sinh Vien: "+idsv);		
+		System.out.println("CMND: "+idnd);		
+		System.out.println("Ten Lop: "+nameclass);		
+		System.out.println("Ten Truong: "+nameschool);		
+		System.out.println("Diem Trung Binh: "+dtb);
+		
+	
+		
+		
+		
+		
+		}
+
+}
